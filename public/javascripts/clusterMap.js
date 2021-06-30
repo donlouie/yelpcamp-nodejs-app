c197b639c0ca178346628ab788f430d033c8e282
donlouie <don_gallegos@outlook.com>
2021-06-30 20:25:45 +0800

    finish Section 56: YelCamp: Fancy Cluster Map

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,7 +1,7 @@
-const gCampgrounds = { features: JSON.parse(campgrounds) };
+const gCampgrounds = campgrounds;
 
 mapboxgl.accessToken = mapToken;
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/dark-v10',
   center: [-103.59179687498357, 40.66995747013945],
@@ -12,7 +12,7 @@ map.on('load', function () {
   // Add a new source from our GeoJSON data and
   // set the 'cluster' option to true. GL-JS will
   // add the point_count property to your source data.
-  map.addSource('earthquakes', {
+  map.addSource('gCampgrounds', {
     type: 'geojson',
     // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
     // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
@@ -25,7 +25,7 @@ map.on('load', function () {
   map.addLayer({
     id: 'clusters',
     type: 'circle',
-    source: 'earthquakes',
+    source: 'gCampgrounds',
     filter: ['has', 'point_count'],
     paint: {
       // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
@@ -49,7 +49,7 @@ map.on('load', function () {
   map.addLayer({
     id: 'cluster-count',
     type: 'symbol',
-    source: 'earthquakes',
+    source: 'gCampgrounds',
     filter: ['has', 'point_count'],
     layout: {
       'text-field': '{point_count_abbreviated}',
@@ -61,7 +61,7 @@ map.on('load', function () {
   map.addLayer({
     id: 'unclustered-point',
     type: 'circle',
-    source: 'earthquakes',
+    source: 'gCampgrounds',
     filter: ['!', ['has', 'point_count']],
     paint: {
       'circle-color': '#11b4da',
@@ -73,12 +73,12 @@ map.on('load', function () {
 
   // inspect a cluster on click
   map.on('click', 'clusters', function (e) {
-    var features = map.queryRenderedFeatures(e.point, {
+    const features = map.queryRenderedFeatures(e.point, {
       layers: ['clusters'],
     });
-    var clusterId = features[0].properties.cluster_id;
+    const clusterId = features[0].properties.cluster_id;
     map
-      .getSource('earthquakes')
+      .getSource('gCampgrounds')
       .getClusterExpansionZoom(clusterId, function (err, zoom) {
         if (err) return;
 
@@ -94,15 +94,8 @@ map.on('load', function () {
   // the location of the feature, with
   // description HTML from its properties.
   map.on('click', 'unclustered-point', function (e) {
-    var coordinates = e.features[0].geometry.coordinates.slice();
-    var mag = e.features[0].properties.mag;
-    var tsunami;
-
-    if (e.features[0].properties.tsunami === 1) {
-      tsunami = 'yes';
-    } else {
-      tsunami = 'no';
-    }
+    const { popUpMarkup } = e.features[0].properties;
+    const coordinates = e.features[0].geometry.coordinates.slice();
 
     // Ensure that if the map is zoomed out such that
     // multiple copies of the feature are visible, the
@@ -111,10 +104,7 @@ map.on('load', function () {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
 
-    new mapboxgl.Popup()
-      .setLngLat(coordinates)
-      .setHTML('magnitude: ' + mag + '<br>Was there a tsunami?: ' + tsunami)
-      .addTo(map);
+    new mapboxgl.Popup().setLngLat(coordinates).setHTML(popUpMarkup).addTo(map);
   });
 
   map.on('mouseenter', 'clusters', function () {
--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,22 +1,17 @@
-const gCampground = JSON.parse(campground);
-
 // <!--Mapbox Script -->
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/light-v10', // style URL
-  //   center: [-74.5, 40], // starting position [lng, lat]
-  //   center: campground.geometry.coordinates,
-  center: gCampground.geometry.coordinates,
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+  center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 10, // starting zoom
 });
 
-// new mapboxgl.Marker().setLngLat([-74.5, 40]).addTo(map);
 new mapboxgl.Marker()
-  .setLngLat(gCampground.geometry.coordinates)
+  .setLngLat(campground.geometry.coordinates)
   .setPopup(
     new mapboxgl.Popup({ offset: 25 }).setHTML(
-      `<h3>${gCampground.title}</h3><p>${gCampground.location}</p>`
+      `<h3>${campground.title}</h3><p>${campground.location}</p>`
     )
   )
   .addTo(map);
